ffa09f6b75f354d4ec45410781d738360bb585ea
Inés Pedraza Pino <inesppino@mbp-de-ines.dhcp.mano.es.sopra>
2019-08-06 13:42:20 +0200

    update docs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 #Flex Panels Image Gallery
-
+Paneles que crecen y muestran frases motivacionales
 
 # Adalab web starter kit
 Uso del kit de Adalab para estructura de carpetas y automatización de tareas
---
+++ docs/assets/js/main.js
@@ -0,0 +1,1 @@
+"use strict";const panels=document.querySelectorAll(".panel");function toggleOpen(){this.classList.toggle("open")}function toggleActive(e){e.propertyName.includes("flex")&&this.classList.toggle("open-active")}panels.forEach(e=>e.addEventListener("click",toggleOpen)),panels.forEach(e=>e.addEventListener("transitionend",toggleActive));
